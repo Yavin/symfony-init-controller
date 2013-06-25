6543990bd09e0c64e786216edfacf1122be92105
Yavin <yavin.yavin@gmail.com>
2013-06-25 08:44:02 +0200

    initial commit

---
+++ src/Yavin/Symfony/Controller/InitControllerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Yavin\Symfony\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface InitControllerInterface
+{
+    public function init();
+}
+
---
+++ src/Yavin/Symfony/Controller/InitControllerSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Yavin\Symfony\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+
+class InitControllerSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'kernel.controller' => 'onKernelController',
+        );
+    }
+
+    /**
+     * call controller init method if controller implements InitControllerInterface
+     *
+     * @param FilterControllerEvent $event
+     */
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        $controller = $event->getController();
+
+        if (!is_array($controller)) {
+            // not a object but a different kind of callable. Do nothing
+            return;
+        }
+
+        $controllerObject = $controller[0];
+        if ($controllerObject instanceof InitControllerInterface) {
+            $controllerObject->init($event->getRequest());
+        }
+    }
+}
+
